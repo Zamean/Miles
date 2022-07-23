8da322bd3b5ddd586670a7098483b18aa14813d1
Zamean <stasann163@gmail.com>
2022-07-23 09:57:22 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPriceKpks;
+        ticketPriceKpks = 20_000;
+        int bonusMiles = ticketPriceKpks / 20 / 100;
+        System.out.println(bonusMiles);
+    }
+}
